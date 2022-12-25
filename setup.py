d4c4793306f90f36599f447b490b41c4c7422a40
Bhargavi Mummareddy <83322691+bmummareddy@users.noreply.github.com>
2022-12-25 17:20:07 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+PACKAGE_NAME = 'bhasum'
+VERSION = "1.1"
+DESCRIPTION = "returns sum"
+KEYWORDS = "addition"
+URL = 'https://github.com/bmummareddy/sum'
+AUTHOR = 'Bhargavi'
+LICENSE = 'Eg'
+REQUIRES_PYTHON = '>=3.7.0'
+EXTRAS = {}
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+with open("requirements.txt", "r") as f:
+    requirements = [line.strip() for line in f.readlines()]
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=KEYWORDS,
+    license=LICENSE,
+    author=AUTHOR,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(include=f"{PACKAGE_NAME}.*"),
+    install_requires=requirements,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Addition: Example package",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
