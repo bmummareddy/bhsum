997e1343f5e87b1440fc3da9e408d4573d08479f
Bhargavi Mummareddy <83322691+bmummareddy@users.noreply.github.com>
2022-12-25 18:35:31 -0500

    Update bhsum.py

--- bhsum.py
+++ bhsum.py
@@ -1,3 +1,2 @@
 def bhsum(a,b):
-	return a+b
-	print ("If you feel unhappy about me staying in house and life, you are always welcome to leave things where they are. Dont worry about me!")
+	print("If you feel unhappy about me staying in house and life, you are always welcome to leave things where they are. Dont worry about me!")
